d15903dea3f802f7ddb38a4dbe12541ff22e19e3
Felipe Vidal <felipevidal@sinaitechnologies.com>
2023-12-28 20:36:16 -0300

    Started web server with simple tracing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,15 @@
-use lopdf::Document;
+use cisa::configuration::get_configuration;
+use cisa::startup::Application;
+use cisa::telemetry::{get_subscriber, init_subscriber};
 
-fn main() {
-    use std::time::Instant;
-    let now = Instant::now();
-    let original_file = "/Users/felipevidal/Downloads/Secure Software Development Attestation Form_508c.pdf";
-    let doc = Document::load(original_file).unwrap();
-    let total_pages = doc.get_pages().len();
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
-    for i in 1..=total_pages {
-        // Reload the original document for each iteration
-        let mut doc = Document::load(original_file).unwrap();
+    let configuration = get_configuration().expect("Failed to read configuration.");
 
-        // Create a vector of pages to delete, excluding the current page
-        let pages_to_delete: Vec<u32> = (1..=total_pages)
-            .filter(|&page| page != i)
-            .map(|page| page as u32)
-            .collect();
-
-        // Delete other pages
-        doc.delete_pages(&pages_to_delete);
-        // Save the current page as a separate PDF
-        let output_file = format!("form/output_page_{}.pdf", i);
-        doc.save(&output_file).unwrap();
-        
-    }
-
-    println!("Elapsed time: {:.2?}", now.elapsed());
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
---
+++ src/startup.rs
@@ -0,0 +1,85 @@
+use crate::configuration::DatabaseSettings;
+use crate::configuration::Settings;
+// use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
+use crate::routes::health_check;
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
+// We need to mark `run` as public.
+// It is no longer a binary entrypoint, therefore we can mark it as async // without having to use any proc-macro incantation.
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    base_url: String,
+) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+    let server = HttpServer::new(move || {
+        // Wrap the connection in a smart pointer
+        App::new()
+            .wrap(TracingLogger::default())
+            .route("/health_check", web::get().to(health_check))
+            // .route("/subscriptions", web::post().to(subscribe))
+            // .route("/subscriptions/confirm", web::get().to(confirm))
+            // .route("/newsletters", web::post().to(publish_newsletter))
+            .app_data(db_pool.clone())
+            // .app_data(email_client.clone())
+            .app_data(base_url.clone())
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
+
+// We need to define a wrapper type in order to retrieve the URL
+// in the `subscribe` handler.
+// Retrieval from the context, in actix-web, is type-based: using
+// a raw `String` would expose us to conflicts.
+pub struct ApplicationBaseUrl(pub String);
+
+pub struct Application {
+    pub port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&configuration.database);
+
+        // Now the port is read from the configuration file.
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(
+            listener,
+            connection_pool,
+            configuration.application.base_url,
+        )?;
+        println!("Server running on port {}", port);
+        // We "save" the bound port in one of `Application`'s fields
+        Ok(Self { port, server })
+    }
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    // A more expressive name that makes it clear that
+    // this function only returns when the application is stopped.
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.with_db())
+}
